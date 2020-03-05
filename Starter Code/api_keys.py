fce92672b42e35b30f227342c8d8e49ad5ca33f7
jassefa <59900725+jassefa@users.noreply.github.com>
2020-03-05 00:00:13 -0600

    final

--- Starter Code/api_keys.py
+++ Starter Code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "18bf7eddca2612d698b1d860d16dbe2d"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyB9Gix2SE21au-sYQlzghncbzvObM-wHio"
